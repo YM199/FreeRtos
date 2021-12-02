4b6a1a8dbebab662c7c5c73c30907fa678f32cc9
YangMou <mr_yang_mou@163.com>
2021-12-02 15:32:39 +0800

    更换为野火的源代码

--- framebuffer/test_app/test_app.c
+++ framebuffer/test_app/test_app.c
@@ -6,59 +6,62 @@
 #include "test_app.h"
 #include <sys/ioctl.h>
 
+/*显示屏相关头文件*/
 #include <linux/fb.h>
 #include <sys/mman.h>
 
 extern unsigned int test_picture[];
 
-/**
- * @brief 
- * 
- * @param fbmem 
- * @param line_length 
- * @param bpp 
- * @param x 
- * @param y 
- * @param color 
- */
-void lcd_show_pixel(unsigned char *fbmem, int line_length, int bpp, int x, int y, unsigned int color)
+void lcd_show_pixel(unsigned char *fbmem, int line_length, int bpp,
+						int x, int y, unsigned int color)
 {
-    unsigned char *p8 = (unsigned char *)(fbmem + y * line_length + x * bpp / 8);
-    unsigned short *p16 = (unsigned short *)p8;
-    unsigned int *p32 = (unsigned int *)p8;
+	unsigned char  *p8  = (unsigned char *)(fbmem + y * line_length + x * bpp / 8);
+	unsigned short *p16 = (unsigned short *)p8;
+	unsigned int   *p32 = (unsigned int *)p8;
 
-    unsigned int red, green, blue;
-    switch (8)
-    {
-    case 8:
-        *p8 = (unsigned char)(color & 0xf);
-        break;
-    case 16:
-        red = (color >> 16) & 0xff;
-        green = (color >> 8) & 0xff;
-        blue = (color >> 0) & 0xff;
-        *p16 = (unsigned short)( ((red >> 3) << 11) | ((green >> 2) << 5) | ((blue >> 3) << 0) );
-        break;
-    case 32:
-        *p32 = color;
-        break;
-    default:
-        printf("can not support &d bpp\r\n");
-        break;
-    }   
+	unsigned int red, green, blue;
+
+	switch(bpp)
+	{
+		case 8:
+		{
+			*p8 = (unsigned char)(color & 0xf);
+			break;
+		}
+
+		case 16:
+		{
+			red   = (color >> 16) & 0xff;
+			green = (color >> 8) & 0xff;
+			blue  = (color >> 0) & 0xff;
+			*p16  = (unsigned short)(((red >> 3) << 11) | ((green >> 2) <<5) | ((blue >> 3) << 0));
+			break;
+		}
+		case 32:
+		{
+			*p32 = color;
+			break;
+		}
+		default:
+		{
+			printf("can not support %d bpp", bpp);
+			break;
+		}
+	}
 }
 
-int main(void)
+
+
+int main()
 {
-    struct fb_var_screeninfo vinfo;
+	struct fb_var_screeninfo vinfo;
     struct fb_fix_screeninfo finfo;
-
-    unsigned int *temp;
-    unsigned int color;
+	unsigned int *temp;
+	unsigned int color;
     unsigned char *fbp = 0;
-    int fp = 0;
-    int x,y;
-
+	int fp = 0;
+	int x, y;
+   
     fp = open("/dev/fb0", O_RDWR);
     if (fp < 0)
     {
@@ -68,13 +71,13 @@ int main(void)
 
     if (ioctl(fp, FBIOGET_FSCREENINFO, &finfo))
     {
-        printf("Error reading fixed infomation\r\n");
+        printf("Error reading fixed information/n");
         exit(2);
     }
 
     if (ioctl(fp, FBIOGET_VSCREENINFO, &vinfo))
     {
-        printf("Error reading variable information\r\n");
+        printf("Error reading variable information/n");
         exit(3);
     }
     printf("The mem is :%d\n", finfo.smem_len);
@@ -83,36 +86,53 @@ int main(void)
     printf("The yres is :%d\n", vinfo.yres);
     printf("bits_per_pixel is :%d\n", vinfo.bits_per_pixel);
 
-    fbp = (unsigned char *) mmap(0, finfo.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fp, 0);
-    if ((int)fbp == -1)
+     /*这就是把fp所指的文件中从开始到screensize大小的内容给映射出来，得到一个指向这块空间的指针*/
+    fbp =(unsigned char *) mmap (0, finfo.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fp,0);
+    if ((int) fbp == -1)
     {
-        printf("Error: fail to map framebuffer device to memory\r\n");
-        exit(4);
+       printf ("Error: failed to map framebuffer device to memory./n");
+       exit (4);
     }
 
-    /*红色*/
-    color = 0xff << 16;
-    for (y = 0; y < vinfo.yres; y++)
-    {
-        for (x = 0; x < vinfo.xres; x++)
-        {
-            lcd_show_pixel(fbp, finfo.line_length, vinfo.bits_per_pixel, x, y, color);
-        }
-    }
-
+    /*刷红色*/
+	color = 0xff << 16; 
+	for (y = 0; y < vinfo.yres; y++)
+	{
+		for(x = 0; x < vinfo.xres; x++)
+			lcd_show_pixel(fbp, finfo.line_length, vinfo.bits_per_pixel, x, y, color);
+	}
     usleep(1000*2000);
-    /*显示图片*/
-    temp = (unsigned int *)test_picture;
-    for (y = 0; y < vinfo.yres; y++)
-    {
-      for(x = 0; x < vinfo.xres; x++)
-      {
-        lcd_show_pixel(fbp, finfo.line_length, vinfo.bits_per_pixel, x, y, *temp);
-        temp++;
-      }
-    }
 
-    munmap(fbp, finfo.smem_len);
+    /*刷绿色*/
+	color = 0xff << 8; 
+	for (y = 0; y < vinfo.yres; y++)
+	{
+		for(x = 0; x < vinfo.xres; x++)
+			lcd_show_pixel(fbp, finfo.line_length, vinfo.bits_per_pixel, x, y, color);
+	}
+    usleep(1000*2000);
+
+    /*刷蓝色*/
+	color = 0xff;
+	for (y = 0; y < vinfo.yres; y++)
+	{
+		for(x = 0; x < vinfo.xres; x++)
+			lcd_show_pixel(fbp, finfo.line_length, vinfo.bits_per_pixel, x, y, color);
+	}
+	usleep(1000*2000);
+	
+    /*显示图片*/
+	temp = (unsigned int *)test_picture;
+	for (y = 0; y < vinfo.yres; y++)
+	{
+		for(x = 0; x < vinfo.xres; x++)
+		{
+			lcd_show_pixel(fbp, finfo.line_length, vinfo.bits_per_pixel, x, y, *temp);
+			temp++;
+		}
+	}
+
+    munmap (fbp, finfo.smem_len); /*解除映射*/
     close(fp);
     return 0;
 }
