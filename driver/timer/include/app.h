5af34038a059f3b85bef6d363785d0e2251407f5
YangMou <mr_yang_mou@163.com>
2022-01-21 17:01:40 +0800

    编译成功

---
+++ driver/timer/include/app.h
@@ -0,0 +1,27 @@
+#ifndef __APP_H__
+#define __APP_H__
+
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "stdlib.h"
+#include "string.h"
+#include "linux/ioctl.h"
+
+#define DEBUG
+
+#ifdef DEBUG 
+    #define debug(...) printf(__VA_ARGS__)
+#else 
+    #define debug(...)
+#endif
+
+#define CLOSE_CMD     (_IO(0XEF, 0X01)) /*关闭定时器*/
+#define OPEN_CMD      (_IO(0xEF, 0X02)) /*打开定时器*/
+#define SETPERIOD_CMD (_IO(0XEF, 0X01)) /*设置定时器周期命令*/
+
+
+
+#endif
