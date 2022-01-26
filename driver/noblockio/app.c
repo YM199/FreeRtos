e768b53d079d1d8813f24c1bd6a65a7d3df95049
YangMou <mr_yang_mou@163.com>
2022-01-26 12:17:26 +0800

    修改app文件

--- driver/noblockio/app.c
+++ driver/noblockio/app.c
@@ -6,29 +6,52 @@ int main( int argc, char **argv )
     int ret = 0;
     int data = 0;
     char *filename;
-
+    fd_set readfds;
+    struct timeval timeout;
     debug( argc < 2 );
 
 
     filename = argv[1];
 
-    fd = open( filename, O_RDWR );
+    fd = open( filename, O_RDWR | O_NONBLOCK); /*非阻塞访问*/
     debug( fd < 0 );
 
 
     while( 1 )
     {
-        ret = read( fd, &data, sizeof( data ) );/*读取数据到data中*/
-        if( ret >= 0 )
+	    FD_ZERO(&readfds);
+	    FD_SET(fd, &readfds);
+	    /* 构造超时时间 */
+	    timeout.tv_sec = 0;
+	    timeout.tv_usec = 500000; /* 500ms */
+	    ret = select(fd + 1, &readfds, NULL, NULL, &timeout);
+	    switch (ret)
         {
-            if( data )
-            {
-                printf( "key value = %d\r\n", data );
-                data = 0;
-            }
-        }
-    }
+		    case 0: 	/* 超时 */
+			/* 用户自定义超时处理 */
+			break;
 
+		    case -1:	/* 错误 */
+			/* 用户自定义错误处理 */
+			break;
+
+		    default:  /* 可以读取数据 */
+			    if(FD_ISSET(fd, &readfds))
+                {
+				    ret = read(fd, &data, sizeof(data));
+				    if (ret < 0)
+                    {
+					    /* 读取错误 */
+				    }
+                    else
+                    {
+					    if (data)
+						    printf("key value=%d\r\n", data);
+				    }
+			    }
+			break;
+	    }
+    }
     close( fd );
     return 0;
 }
