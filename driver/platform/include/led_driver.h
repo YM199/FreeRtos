107d6412818d543bf0d953ac535bfecbb2e2b6a0
YangMou <mr_yang_mou@163.com>
2022-02-09 16:33:04 +0800

    更新头文件

--- driver/platform/include/led_driver.h
+++ driver/platform/include/led_driver.h
@@ -1,11 +1,45 @@
 #ifndef __LED_DRIVER_H__
 #define __LED_DRIVER_H__
 
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/ide.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/gpio.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/of_gpio.h>
+#include <linux/semaphore.h>
+#include <linux/timer.h>
+#include <linux/irq.h>
+#include <linux/wait.h>
+#include <linux/poll.h>
+#include <linux/fs.h>
+#include <linux/fcntl.h>
+#include <linux/platform_device.h>
+#include <asm/mach/map.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
 
-struct xxx_dev{
-    struct cdev cdev;
+struct led_dev{
+    dev_t              devid;  /*设备号*/
+    struct cdev        cdev;   /*字符设备*/
+    struct class       *class; /*类*/
+    struct device      *device; /*设备*/
+    int                major;  /*主设备号*/
+    int                minor;  /*次设备号*/
+    struct device_node *node;  /*LED设备节点*/
+    int                led0;   /*LED灯的GPIO编号*/
 };
 
+
+#define DEV_CNT  1
+#define DEV_NAME "led"
+#define LED_OFF  0
+#define LED_ON   1
 
 #define DEBUG
 #ifdef  DEBUG
