18c21568b745af137186721fa668e0f6acef41a3
YangMou <mr_yang_mou@163.com>
2022-02-18 18:36:21 +0800

    合并文件

--- driver/touch/gt9147.c
+++ driver/touch/gt9147.c
@@ -1,5 +1,46 @@
+#include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/regmap.h>
+#include <linux/gpio/consumer.h>
+#include <linux/of_irq.h>
+#include <linux/interrupt.h>
+#include <linux/input.h>
+#include <linux/input/mt.h>
+#include <linux/debugfs.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+#include <linux/gpio.h>
+#include <linux/of_gpio.h>
+#include <linux/input/mt.h>
+#include <linux/input/touchscreen.h>
+#include <linux/i2c.h>
 
-#include "include/gt9147.h"
+#define GT_CTRL_REG 	        0X8040  /* GT9147控制寄存器         */
+#define GT_MODSW_REG 	        0X804D  /* GT9147模式切换寄存器        */
+#define GT_CFGS_REG 	        0X8047  /* GT9147配置起始地址寄存器    */
+#define GT_CHECK_REG 	        0X80FF  /* GT9147校验和寄存器       */
+#define GT_PID_REG 		        0X8140  /* GT9147产品ID寄存器       */
+
+#define GT_GSTID_REG 	        0X814E  /* GT9147当前检测到的触摸情况 */
+#define GT_TP1_REG 		        0X814F  /* 第一个触摸点数据地址 */
+#define GT_TP2_REG 		        0X8157	/* 第二个触摸点数据地址 */
+#define GT_TP3_REG 		        0X815F  /* 第三个触摸点数据地址 */
+#define GT_TP4_REG 		        0X8167  /* 第四个触摸点数据地址  */
+#define GT_TP5_REG 		        0X816F	/* 第五个触摸点数据地址   */
+#define MAX_SUPPORT_POINTS      5       /* 最多5点电容触摸 */
+
+#define LCD_X 800
+#define LCD_Y 480
+
+#define FIREWARE 0  /*芯片内置固件已能够使用，无需下载固件*/
+
+struct gt9147_dev {
+	int irq_pin,reset_pin;					/* 中断和复位IO		*/
+	int irqnum;								/* 中断号    		*/
+	void *private_data;						/* 私有数据 		*/
+	struct input_dev *input;				/* input结构体 		*/
+	struct i2c_client *client;				/* I2C客户端 		*/
+};
 
 
 struct gt9147_dev gt9147; /*代表gt9147这个设备*/
