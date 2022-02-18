d636a4a7caccaacbe5f27a8f88364b38ab60c7f1
YangMou <mr_yang_mou@163.com>
2022-02-18 18:02:57 +0800

    更新头文件

--- driver/touch/app.c
+++ driver/touch/app.c
@@ -9,23 +9,7 @@
  日志 : 初版 V1.0 2021/6/15 邓涛创建
  ***************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <string.h>
-#include <linux/input.h>
-
-/* 用于描述MT多点触摸每一个触摸点的信息 */
-struct ts_mt {
-    int x;      	//X坐标
-    int y;      	//Y坐标
-    int id;     	//对应ABS_MT_TRACKING_ID
-    int valid;		//数据有效标志位(=1表示触摸点信息发生更新)
-};
+#include "include/app.h"
 
 /* 一个触摸点的x坐标和y坐标 */
 struct tp_xy {
